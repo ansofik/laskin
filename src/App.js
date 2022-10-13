d925e8ae8852cbdbac2388ff8ed75905a2df56b8
sofia <84174004+sofiak0@users.noreply.github.com>
2022-10-13 15:32:55 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react"
+import Nappain from './Nappain';
+
+//let nappaimet = ["0","1","2","3","4","5","6","7","8","9",".","+-","+", "-", "*","/", "=","clear"];
+let nappaimet = ["7","8","9","/","4","5","6","*","1","2","3","-","0",".","+-","+","=","clear"];
 
 function App() {
+  const [teksti, setTeksti] = useState("");
+  //console.log("tila", tila)
+  // tila = true //setTila (false)
+  const nappainPainettu = (x) => {
+    if (x == "=" && teksti.length !== 0) {
+      setTeksti(eval(teksti));
+      return;
+    }
+
+    if (x == "=") {
+      return;
+    }
+
+    if (x == "clear") {
+      setTeksti("");
+      return;
+    }
+
+    // vaihda etumerkkiä
+    if (x == "+-") {
+      if (teksti[0] == "-") {
+          setTeksti(teksti.slice(1))
+        } else setTeksti("-" + teksti);
+      return;
+      }
+
+    // ei kahta merkkiä peräkkäin
+    const merkit = [".","+","-","*","/"]
+    if (merkit.includes(teksti[teksti.length-1]) && merkit.includes(x)){
+      return;
+    }
+    
+    setTeksti(teksti+x);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='koko'>
+      <div className='naytto'>
+        <p>{teksti}</p>
+      </div>
+      <div className='nappaimet'>
+        {nappaimet.map((nappain, index) => <Nappain key={index} nappainPainettu={nappainPainettu} nappain={nappain} />)}
+      </div>
     </div>
   );
 }
---
+++ src/Nappain.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import {useState} from "react"
+
+function Nappain(props) {
+  return (
+        <button onClick={() => props.nappainPainettu(props.nappain)}>{props.nappain}</button>
+  );
+}
+
+export default Nappain; 
