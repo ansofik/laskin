7aa52f84702efa1a0f29335099ccd63bdaa016ef
sofia <84174004+sofiak0@users.noreply.github.com>
2022-10-14 08:58:30 +0300

    Added a button to switch the background image

--- src/App.js
+++ src/App.js
@@ -3,30 +3,30 @@ import {useState} from "react"
 import Nappain from './Nappain';
 
 //let nappaimet = ["0","1","2","3","4","5","6","7","8","9",".","+-","+", "-", "*","/", "=","clear"];
-let nappaimet = ["7","8","9","/","4","5","6","*","1","2","3","-","0",".","+-","+","=","clear"];
+let nappaimet = ["7","8","9","/","4","5","6","*","1","2","3","-","0",".","+-","+","nollaa","="];
 
 function App() {
   const [teksti, setTeksti] = useState("");
   //console.log("tila", tila)
   // tila = true //setTila (false)
   const nappainPainettu = (x) => {
-    if (x == "=" && teksti.length !== 0) {
+    if (x === "=" && teksti.length !== 0) {
       setTeksti(eval(teksti));
       return;
     }
 
-    if (x == "=") {
+    if (x === "=") {
       return;
     }
 
-    if (x == "clear") {
+    if (x === "nollaa") {
       setTeksti("");
       return;
     }
 
     // vaihda etumerkkiä
-    if (x == "+-") {
-      if (teksti[0] == "-") {
+    if (x === "+-") {
+      if (teksti[0] === "-") {
           setTeksti(teksti.slice(1))
         } else setTeksti("-" + teksti);
       return;
@@ -41,7 +41,14 @@ function App() {
     setTeksti(teksti+x);
   }
 
+  const [tausta, setTausta] = useState("green");
+
+  const muutaTausta = () => {
+    setTausta(tausta==="green" ? "yellow" : "green");
+  }
+
   return (
+    <div className={'fill ' + tausta}>
     <div className='koko'>
       <div className='naytto'>
         <p>{teksti}</p>
@@ -49,6 +56,8 @@ function App() {
       <div className='nappaimet'>
         {nappaimet.map((nappain, index) => <Nappain key={index} nappainPainettu={nappainPainettu} nappain={nappain} />)}
       </div>
+      <div><button className={'styleButton ' + tausta} onClick={muutaTausta}></button></div>
+    </div>
     </div>
   );
 }
